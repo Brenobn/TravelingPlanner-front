437ca8e7aabe48898118235c3ee664f332fe6dae
Brenobn <brenobragabn@gmail.com>
2024-07-08 23:08:48 -0300

    Adjusting MapPin icon spacing

--- src/app.tsx
+++ src/app.tsx
@@ -11,7 +11,7 @@ export function App() {
 
         <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
           <div className='flex items-center gap-2 flex-1'>
-            <MapPin className='size-5 text-zinc-400 flex-1'/>
+            <MapPin className='size-5 text-zinc-400'/>
             <input type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none " />
           </div>
 
