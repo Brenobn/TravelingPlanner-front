19405314d017dc8f8ee31d6c9825c8f389ed5650
Brenobn <brenobragabn@gmail.com>
2024-07-09 09:32:37 -0300

    Creating a verification structure and adding another button to the input checking if the isGuestsInput state is set

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { MapPin, Calendar, ArrowRight, UserRoundPlus } from 'lucide-react'
+import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2 } from 'lucide-react'
 import { useState } from 'react'
 
 export function App() {
@@ -20,20 +20,27 @@ export function App() {
           <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
             <div className='flex items-center gap-2 flex-1'>
               <MapPin className='size-5 text-zinc-400'/>
-              <input type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none " />
+              <input disabled={isGuestsInputOpen} type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none " />
             </div>
 
             <div className='flex items-center gap-2'>
               <Calendar className='size-5 text-zinc-400' />
-              <input type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none " />
+              <input disabled={isGuestsInputOpen} type="text" placeholder="Quando?" className="bg-transparent text-lg placeholder-zinc-400 w-40 outline-none " />
             </div>
 
             <div className='w-px h-6 bg-zinc-400' />
 
-            <button onClick={openGuestsInput} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
-              Continuar
-              <ArrowRight className='size-5 ' />
-            </button>
+            {isGuestsInputOpen ? (
+              <button className='bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700'>
+                Alterar local/data
+                <Settings2 className='size-5'/>
+              </button>
+            ) : (
+              <button onClick={openGuestsInput} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
+                Continuar
+                <ArrowRight className='size-5 ' />
+              </button>
+            )}
           </div>
 
           {isGuestsInputOpen && (
